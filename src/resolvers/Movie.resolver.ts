c2ec97ff0a65952030cd48197fdbff858f65e376
Aman Gupta <amang5840@gmail.com>
2021-09-09 22:38:52 +0530

    Update a movie

--- src/resolvers/Movie.resolver.ts
+++ src/resolvers/Movie.resolver.ts
@@ -1,27 +1,51 @@
 import { Movie } from "../entity/Movie";
-import { Arg, Field, InputType, Int, Mutation, Query, Resolver } from "type-graphql";
+import {
+  Arg,
+  Field,
+  InputType,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+} from "type-graphql";
 
 @InputType()
-class MovieType{
-    @Field()
-    title: string
+class MovieInput {
+  @Field()
+  title: string;
 
-    @Field(() => Int)
-    length: number
+  @Field(() => Int)
+  length: number;
+}
+
+@InputType()
+class MovieUpdateInput {
+  @Field(() => String, { nullable: true })
+  title?: string;
+
+  @Field(() => Int, { nullable: true })
+  length?: number;
 }
 
 @Resolver()
-export class MovieResolver  {
+export class MovieResolver {
   @Mutation(() => Movie)
-  async createMovie(
-    @Arg("options") options: MovieType
-  ) {
-     const movie = await Movie.create(options).save()
+  async createMovie(@Arg("options") options: MovieInput) {
+    const movie = await Movie.create(options).save();
     return movie;
   }
 
+  @Mutation(() => Boolean)
+  async updateMovie(
+    @Arg("id", () => Int) id: number,
+    @Arg("input", () => MovieUpdateInput) input: MovieUpdateInput
+  ) {
+    await Movie.update(id, input);
+    return true;
+  }
+
   @Query(() => [Movie])
-  getMovies(){
-      return Movie.find()
+  getMovies() {
+    return Movie.find();
   }
 }
